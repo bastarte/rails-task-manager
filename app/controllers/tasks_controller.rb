4be0ef4f27a505a54ff71eb0f4e443b0520e4aa8
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-02-23 16:23:17 +0100

    update a task, delete a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,24 @@ class TasksController < ApplicationController
     redirect_to task_path(@task.id)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   post "tasks", to: "tasks#create"
   get "tasks/new", to: "tasks#new", as: :new
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
   get "tasks/:id", to: "tasks#show", as: :task
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
+  patch "tasks/:id", to: "tasks#update"
 end
